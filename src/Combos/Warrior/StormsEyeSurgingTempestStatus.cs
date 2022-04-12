ef37140cd538ab37d4c694df20750104485bac7f
羽先生 <8655163+VergilGao@users.noreply.github.com>
2022-04-12 16:06:20 +0800

    续红斩

---
+++ src/Combos/Warrior/StormsEyeSurgingTempestStatus.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * filename: StormsEyeSurgingTempestStatus.cs
+ * maintainer: VergilGao
+ * description:
+ *  暴风碎战场风暴状态
+ */
+
+using CustomComboPlugin.Attributes;
+
+namespace CustomComboPlugin.Combos.Warrior
+{
+    /// <summary>
+    /// 暴风碎战场风暴状态
+    /// </summary>
+    [SecretCombo]
+    [CustomComboInfo("战场风暴状态", "红斩不足15秒时，暴风碎替换暴风斩", Job.Warrior, Identity, 4)]
+    internal sealed class StormsEyeSurgingTempestStatus : CustomCombo
+    {
+        public const ushort Identity = (Job.Warrior << 8) ^ 0x06;
+
+        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
+        {
+            if (actionID == Warrior.Identities.Skills.StormsPath)
+            {
+                if (comboTime > 0)
+                {
+                    if (lastComboMove == Warrior.Identities.Skills.Maim &&
+                        level >= Warrior.Identities.Levels.StormsEye &&
+                        !(FindPlayerEffect(Warrior.Identities.Buffs.SurgingTempest)?.RemainingTime > 15))
+                    {
+                        return Warrior.Identities.Skills.StormsEye;
+                    }
+                }
+            }
+
+            return actionID;
+        }
+    }
+}
