02f770b50ddc25a8cf03b0a60c6da0377d145164
羽先生 <8655163+VergilGao@users.noreply.github.com>
2022-04-12 17:45:08 +0800

    蛮荒崩裂预备状态

---
+++ src/Combos/Warrior/PrimalRendReadyStatus.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * filename: PrimalRendReadyStatus.cs
+ * maintainer: VergilGao
+ * description:
+ *  蛮荒崩裂预备状态
+ */
+
+using CustomComboPlugin.Attributes;
+
+namespace CustomComboPlugin.Combos.Warrior
+{
+    /// <summary>
+    /// 蛮荒崩裂预备状态
+    /// </summary>
+    [CustomComboInfo("蛮荒崩裂预备状态", "蛮荒崩裂预备状态时，蛮荒崩裂替换解放", Job.Warrior, Identity, 5)]
+    internal sealed class PrimalRendReadyStatus : CustomCombo
+    {
+        public const ushort Identity = (Job.Warrior << 8) ^ 0x13;
+
+        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
+        {
+            if (actionID == Warrior.Identities.Skills.Berserk ||
+                actionID == Warrior.Identities.Skills.InnerRelease)
+            {
+                if (FindPlayerEffect(Warrior.Identities.Buffs.PrimalRendReady) is not null)
+                {
+                    return Warrior.Identities.Skills.PrimalRend;
+                }
+            }
+
+            return actionID;
+        }
+    }
+}
